33a8972dad04d7f4b592f160e7154c3049dd917b
helua <helua@users.noreply.github.com>
2021-03-30 19:52:19 +0200

    gitignore module neutral before universal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -47,7 +47,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     VideoDetailsComponent,
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'andiamo' }),
+    BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
