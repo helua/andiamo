62d43a23991ca887e1b9c11290cc7f8862c18869
helua <helua@users.noreply.github.com>
2022-02-01 22:18:13 +0100

    scully config extra routes edited 2

--- scully.andiamo.config.ts
+++ scully.andiamo.config.ts
@@ -7,8 +7,8 @@ export const config: ScullyConfig = {
   },
   extraRoutes: [
     // '/newsy/andiamo-new-website',
-    '/','/o-nas','/z-planu','kontakt','soulreel',
-    '/en','/about','/bts','/contact','soulreel-en',
+    '/','/o-nas','/z-planu','/kontakt','/soulreel',
+    '/en','/about','/bts','/contact','/soulreel-en',
     '/videos/475158192','/videos/582470156','/videos/564673466','/videos/378886789','/videos/548741050','/videos/520875253','/videos/500000455','/videos/315081982','/videos/314899914','/videos/314838458','/videos/367737286','/videos/302792712','/videos/354766030','/videos/432297620','/videos/435996487','/videos/444232342','/videos/394249129','/videos/455748813','/videos/541561567','/videos/452512519','/videos/465926823','/videos/465925486','/videos/293498910','/videos/349837985'
   ]
 };
